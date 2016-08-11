3942153c945fce74cedcfa8a16ef7ed4b9e10049
fy <fanyang_32012@outlook.com>
2016-08-11 15:57:14 +0800

    Masonry应用中如何体现函数式&链式编程思想

--- Functional-Programming/Functional-Programming/ViewController.m
+++ Functional-Programming/Functional-Programming/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import <Masonry.h>
+
 @interface ViewController ()
 
 @end
@@ -17,11 +19,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self createMas];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)createMas{
+    
+    UIView * view = [[UIView alloc]init];
+    
+    [self.view addSubview:view];
+    
+    view.backgroundColor = [UIColor greenColor];
+/*
+ Masonry能非常好的体现出函数式编程和链式编程的思想以及做法
+ 
+ 链式编程就像Masonry中,make能够一直点下去一样,至于怎么做我们后面会详细讲到他的原理很简单类似于这样
+ 
+ id obj1 = [make centerX];
+ id obj2 = [obj1 equalTo()];
+ 
+ 每次执行完都会有一个返回值,这个返回值是个对象,一般是返回'类'自己本身的对象
+ 
+ 函数式编程思想就像equalTo()一样,OC中我们一般使用方括号表示执行方法,()一般表示执行block,所以我们这里一般指的是执行一个block,且传入的参数是self.view
+ 
+ */
+    [view mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.centerY.equalTo(self.view);
+        make.size.mas_offset(CGSizeMake(100, 100));
+    }];
 }
 
 @end
